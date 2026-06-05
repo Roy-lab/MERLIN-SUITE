c51fedf032c17941fd2337cc60640ad56d286fff
SuvojitHazra <129977598+SuvojitHazra@users.noreply.github.com>
2026-06-05 15:15:01 +0530

    Update AUPR_Fscore_plotting.sh

--- scripts/AUPR_Fscore_plotting.sh
+++ scripts/AUPR_Fscore_plotting.sh
@@ -1,7 +1,7 @@
 export HMAWK=Heatmap.awk
 
 #Color map
-COLORMAP="0.00:(148,0,211);0.02:(75,0,130);0.04:(0,0,255);0.06:(0,255,255);0.08:(0,255,0);0.10:(255,255,0);0.12:(255,127,0);0.16:(255,0,0)"
+COLORMAP="0.00:(148,0,211);0.04:(75,0,130);0.08:(0,0,255);0.12:(0,255,255);0.16:(0,255,0);0.20:(255,255,0);0.24:(255,127,0);0.28:(255,0,0)"
 
 #Making AUPR plot
 cat results/aupr_results/plot_input.txt | ${HMAWK} -vStrokeC="-" -vStrokeSC="black" -vL="Value" -vD="space" -vC="${COLORMAP}" -vFontSize=8 > results/aupr_results/AUPR_Fscore.svg
